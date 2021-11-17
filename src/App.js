10462daaae34e0275a0c5450ada7e06ede479c0a
Sumanbitw <007sumanghosh@gmail.com>
2021-11-17 14:43:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Login from './features/authentication/Login';
+import { Routes, Route } from "react-router"
+import Signup from './features/authentication/Signup';
+import Profile from './features/authentication/Profile';
+import VerifyOTP from './features/authentication/VerifyOTP';
+
+
+  /*
+  1.Login
+  2.verify
+  3.signup isLogin=false
+  4.profile
+  5.logout
+  */
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/verify" element={<VerifyOTP/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+    </Routes>
     </div>
   );
 }
