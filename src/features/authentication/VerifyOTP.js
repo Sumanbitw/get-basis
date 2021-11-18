01e26ed16743c53abe92902fe00995f007430dff
Sumanbitw <007sumanghosh@gmail.com>
2021-11-18 12:33:59 +0530

    refactor and removed console log

--- src/features/authentication/VerifyOTP.js
+++ src/features/authentication/VerifyOTP.js
@@ -1,60 +1,78 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
-import "../../assests/verify.css"
+import "../../assests/verify.css";
 import { resetData, verifyOtpAndToken } from "./loginSlice";
-import { AiOutlineArrowLeft } from "react-icons/ai"
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
 function VerifyOTP() {
   const login = useSelector((state) => state.login);
   const [verifyOtp, setVerifyOtp] = useState("");
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if(login.step !== 2){
-      dispatch(resetData())
-      navigate("/")
+    if (login.step !== 2) {
+      dispatch(resetData());
+      navigate("/");
     }
-  },[])
+  }, []);
 
   const handleVerifyOtpChange = (e) => {
-    const { value } = e.target
-    if(value!== ""){
+    const { value } = e.target;
+    if (value !== "") {
       setVerifyOtp(value);
     }
   };
-  function sendOtp(){
-    let inputValues = {email : login.email, token : login?.token, verificationCode : verifyOtp }
-    return {...inputValues}
+  function sendOtp() {
+    let inputValues = {
+      email: login.email,
+      token: login?.token,
+      verificationCode: verifyOtp,
+    };
+    return { ...inputValues };
   }
-  const handleVerifyClick = async(e) => {
+  const handleVerifyClick = async (e) => {
     e.preventDefault();
-    try{
-      await dispatch(verifyOtpAndToken(sendOtp()))
-      if(login.isLogin === false && verifyOtp === "112233"){
-        navigate("/signup")
-      }else if(login.isLogin === true && verifyOtp === "112233"){
-        navigate("/profile")
-      }else{
-        navigate("/verify")
+    try {
+      await dispatch(verifyOtpAndToken(sendOtp()));
+      if (login.isLogin === false && verifyOtp === "112233") {
+        navigate("/signup");
+      } else if (login.isLogin === true && verifyOtp === "112233") {
+        navigate("/profile");
+      } else {
+        navigate("/verify");
       }
-    }catch(error){}
+    } catch (error) {}
   };
-
-
+  console.log(login?.wrongEmailTokenCount)
   return (
     <div className="verify">
       <div className="verify__container">
-        <input
-          type="text"
-          placeholder="Enter a valid OTP"
-          onChange={handleVerifyOtpChange}
-        />
-        <div>{login?.success === false && <p>{login?.message}</p>}</div>
-        <button className="button" onClick={handleVerifyClick}>Verify</button>
+        <div className="verify__header">
+          <h1>Verify email</h1>
+        </div>
+        <div className="verify__input">
+          <input
+            type="text"
+            placeholder="Enter a valid OTP"
+            onChange={handleVerifyOtpChange}
+          />
+        </div>
+        <div className="verify__error">
+          {login?.success === false && <p>{login?.message}</p>}
+        </div>
+        <div className="button__container">
+          <button className="button__verify" onClick={handleVerifyClick}>
+            Verify
+          </button>
+        </div>
+        <div className="arrow">
+        {login?.wrongEmailTokenCount === false && (
+          <AiOutlineArrowLeft onClick={() => navigate("/")} size={28}/>
+        )}
       </div>
-      <div>{login?.wrongEmailTokenCount === false && <AiOutlineArrowLeft onClick={() => navigate("/")}/>}</div>
+      </div>
     </div>
   );
 }
