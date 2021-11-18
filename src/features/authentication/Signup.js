770a56ce01db4846a5112ac3dd46a13ca94600c3
Sumanbitw <007sumanghosh@gmail.com>
2021-11-18 16:46:25 +0530

    refactor signup

--- src/features/authentication/Signup.js
+++ src/features/authentication/Signup.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { postSignUpDetails, resetData } from "./loginSlice"
+import { postSignUpDetails, resetData } from "./loginSlice";
 import "../../assests/signup.css";
 import { useNavigate } from "react-router";
 
@@ -11,16 +11,17 @@ function Signup() {
   const [agreeToPrivacy, setAgreeToPrivacy] = useState(false);
   const [referredCodeKey, setReferredCodeKey] = useState("");
   const [referral, setReferral] = useState(null);
+  const [message, setMessage] = useState(false);
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if(signup.step !== 3){
-      dispatch(resetData())
-      navigate("/")
+    if (signup.step !== 3) {
+      dispatch(resetData());
+      navigate("/");
     }
-  },[])
- 
+  }, []);
+
   const handleFirstNameChange = (e) => {
     setFirstName(e.target.value);
   };
@@ -48,24 +49,25 @@ function Signup() {
       referredCodeKey: referredCodeKey,
       agreeToPrivacyPolicy: agreeToPrivacy,
       token: signup?.token,
-      source: "WEB_APP"
+      source: "WEB_APP",
     };
-    return {...inputValues};
+    return { ...inputValues };
   }
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try{
-      if(firstName !== "" && email !== "" && agreeToPrivacy === true){
-        await dispatch(postSignUpDetails(sendSignupDetails()))
-      }     
-    }catch(error){}  
+    try {
+      if (firstName !== "" && email !== "" && agreeToPrivacy === true) {
+        await dispatch(postSignUpDetails(sendSignupDetails()));
+        setMessage(true);
+      } else {
+        setMessage(false);
+      }
+    } catch (error) {}
   };
 
-  console.log(agreeToPrivacy);
   return (
     <div className="form">
       <form className="form__container" onSubmit={handleSubmit}>
-        <div className="signup__header"><h1>Sign up</h1></div>
         <input
           type="text"
           placeholder="Enter your first name"
@@ -84,13 +86,20 @@ function Signup() {
           id="input"
           onChange={handleReferralCodeKey}
         />
-        <div className="signup__error error">{!referral && <p>Please enter a valid code or else leave empty</p>}</div>
-        <div className="signup__error">{referral && <p>valid referral code</p>}</div>
+        <div className="signup__error">
+          {message && <p>{signup?.message}</p>}
+        </div>
+        <div className="signup__error">
+          {!referral && <p>Please enter a valid code or else leave empty</p>}
+        </div>
+        <div className="signup__error">
+          {referral && <p>valid referral code</p>}
+        </div>
         <label>
           <input type="checkbox" onClick={handleAgreeToPrivacy} />
           *Agree to privacy
         </label>
-        <input className="button__signup" value="Sign up" type="submit"/>
+        <input className="button__signup" value="Sign up" type="submit" />
       </form>
     </div>
   );
